d29ca3a9d266f8051eb70e5538ac1b1bdc7b9367
elmoche150 <pperezm@fi.uba.ar>
2025-12-17 18:53:42 -0300

    boostrap funcional, en especial en el header

--- front/app.py
+++ front/app.py
@@ -8,4 +8,4 @@ def index():
     return render_template('index.html')
 
 if __name__  == "__main__":
-    app.run("127.0.0.1", port= "5002", debug=True)
+    app.run("127.0.0.1", port= "5003", debug=True)
