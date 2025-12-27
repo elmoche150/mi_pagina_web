7f417ffea16914f1311934da6f47bb383962ee4d
elmoche150 <pperezm@fi.uba.ar>
2025-12-26 22:22:37 -0300

    añadi blueprints para poder modularizar la pgina

--- front/app.py
+++ front/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 import os
 
+from blueprints.index.index import index_bp
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+
+app.register_blueprint(index_bp, url_prefix="/")
+
 
 if __name__  == "__main__":
-    app.run("127.0.0.1", port= "5003", debug=True)
+    app.run("localhost", port= "5003", debug=True)
